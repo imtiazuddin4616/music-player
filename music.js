e54641349cd7244dcbc75e08ab59b54e01ca228b
imtiaz4616 <imtiazuddinedu@gmail.com>
2023-10-20 00:16:07 +0600

    update

--- music.js
+++ music.js
@@ -3,7 +3,6 @@ let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon=document.getElementById("ctrlIcon");
 
-
 song.onloadedmetadata =function(){
     progress.max=song.duration;
     progress.value=song.currentTime;
